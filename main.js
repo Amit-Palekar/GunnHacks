0b78da8b6109b95e7e218c17892e05d06495b4da
Amit Palekar <amit12.palekar@gmail.com>
2017-10-28 01:33:25 -0700

    Add stuff

--- main.js
+++ main.js
@@ -1,5 +1,70 @@
+var map;
+window.onload = function() {
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyDJfecggDCSSnp1SS265blDqR182s2ZywE",
+    authDomain: "gunnhacks-1509167013046.firebaseapp.com",
+    databaseURL: "https://gunnhacks-1509167013046.firebaseio.com",
+    projectId: "gunnhacks-1509167013046",
+    storageBucket: "gunnhacks-1509167013046.appspot.com",
+    messagingSenderId: "7288610574"
+  };
+  firebase.initializeApp(config);
+
+
+};
+
+function initAutocomplete() {
+  var input = document.getElementById('pac-input');
+  console.log(input);
+  var searchBox = new google.maps.places.SearchBox(input);
+  map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+
+  // Bias the SearchBox results towards current map's viewport.
+  map.addListener('bounds_changed', function() {
+    searchBox.setBounds(map.getBounds());
+  });
+
+  // Listen for the event fired when the user selects a prediction and retrieve
+  // more details for that place.
+  searchBox.addListener('places_changed', function() {
+    var places = searchBox.getPlaces();
+
+    if (places.length == 0) {
+      return;
+    }
+    // For each place, get the icon, name and location.
+    var bounds = new google.maps.LatLngBounds();
+    places.forEach(function(place) {
+      if (!place.geometry) {
+        console.log("Returned place contains no geometry");
+        return;
+      }
+      var icon = {
+        url: place.icon,
+        size: new google.maps.Size(71, 71),
+        origin: new google.maps.Point(0, 0),
+        anchor: new google.maps.Point(17, 34),
+        scaledSize: new google.maps.Size(25, 25)
+      };
+
+      if (place.geometry.viewport) {
+        // Only geocodes have viewport.
+        bounds.union(place.geometry.viewport);
+      } else {
+        bounds.extend(place.geometry.location);
+      }
+    });
+    map.fitBounds(bounds);
+  });
+}
+
+function addDestination() {
+  var ref = firebase.db().ref('unassigned');
+}
+
 function initMap() {
-  var map = new google.maps.Map(document.getElementById('map'), {
+  map = new google.maps.Map(document.getElementById('map'), {
     center: {lat: -34.397, lng: 150.644},
     zoom: 2
   });
